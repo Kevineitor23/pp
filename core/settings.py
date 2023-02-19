add6a0823790d9623cbf0d741f6f7707a9f5bf38
KevinCorredor <kevincorredor10@gmail.com>
2023-02-19 18:30:23 -0500

    kak

--- core/settings.py
+++ core/settings.py
@@ -15,6 +15,7 @@ if RENDER_EXTERNAL_HOSTNAME:
     
 # Application definition
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
